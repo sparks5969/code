8ca7e3cc74f230693a4124b83d20162ee573b559
sparks5969 <sxw924@case.edu>
2024-04-03 23:47:53 -0400

    searh examples from student essay, and then embedding

---
+++ text_search.py
@@ -0,0 +1,37 @@
+from fuzzywuzzy import fuzz
+import pandas as pd
+import os
+import json
+from tqdm import tqdm
+
+def find_sentences_with_keywords(sentences, example_keywords, threshold=80):
+    example_sentences = []
+    for sentence in sentences:
+        for keyword in example_keywords:
+            if fuzz.partial_ratio(keyword, sentence.lower()) >= threshold:
+                example_sentences.append(sentence)
+                break
+    return example_sentences
+
+
+# keywords to search for
+example_keywords = ["for example", "for instance", "an example of", "illustrate", "example", "such as"]
+
+# create a dataframe to store search results. # first column: essay_id, second column: example_sentences
+example_sentences_df = pd.DataFrame(columns=["essay_id", "example sentences"])
+
+# iterate through the folder "rawtext" and search for example sentences
+for filename in tqdm(os.listdir("rawtext")):
+    # remove the file extension
+    essay_id = filename.split(".")[0]
+    # open each file and search for example sentences
+    with open("test/" + filename, "r") as file:
+        essay = json.load(file)
+        example_sentences = find_sentences_with_keywords(essay, example_keywords)
+        # add the search results to the dataframe, ignore index
+        df = pd.DataFrame({'essay_id': essay_id, 'example sentences': example_sentences})
+        # merge df with the example_sentences_df dataframe
+        example_sentences_df = pd.concat([example_sentences_df, df], ignore_index=True)
+
+# save the search results to a csv file
+example_sentences_df.to_csv("example_sentences.csv", index=False)
